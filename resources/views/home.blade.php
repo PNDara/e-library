3556a44cf8afeac27eb55b80459ba205ccb09e93
Phin Dara <dara.phin@student.cadt.edu.kh>
2024-01-28 14:41:36 +0700

    Initialize project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,33 @@
+<x-layout>
+<br>
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="https://t4.ftcdn.net/jpg/04/83/71/95/240_F_483719567_tRNWJWCE2H2nKAEqEQjStfhBbqrfBkaJ.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="https://as1.ftcdn.net/v2/jpg/03/89/86/92/1000_F_389869283_x5A0j7f0biTBFKgzLlwt33FXM5QiYEgM.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="https://t4.ftcdn.net/jpg/05/26/31/19/240_F_526311939_DVS9EEQxMkg5NBmMG3tVbLHtUz9pYOuj.jpg" class="d-block w-100" alt="...">
+          </div>
+        </div>
+      </div><br>
+    
+      <div class="card">
+        <div class="card-body bg-info text-dark w-99 h-99 text-center-center">
+          This is some text within a card body.
+        </div>
+      </div><br>
+
+      <div class="card" style="width: 18rem; position-absolute start-10">
+        <img src="https://scontent.fpnh10-1.fna.fbcdn.net/v/t1.6435-9/103839433_3897851450286087_5869508311144037234_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=7f8c78&_nc_ohc=6XopWgHhTEAAX9UBgnH&_nc_oc=AQk_zevGlDyM6zkEkI_S2SODu48LP7d7BJzTFmycnK_D5dVwvc49UqyKRNPtsBgOTKk&_nc_ht=scontent.fpnh10-1.fna&oh=00_AfAHz8mOzvYMgWPO_Cv8ycZpK7dyroN2AKiZuFjNVGho8A&oe=65DD7CC8" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Hair Cut</h5>
+          <p class="card-text">Everything</p>
+          <a href="#" class="btn btn-primary">See More</a>
+        </div>
+      </div>
+
+      
+</x-layout>
